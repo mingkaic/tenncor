362d7b9afa838b0069e4b80530b71d51c15dff9a
Unknown <rododorodo@gmail.com>
2018-09-20 23:19:43 -0700

    fix listcov script

--- listcov.sh
+++ listcov.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+# generate and get coverage.dat paths according to arguments
 PATHS_STR=$(make $1 2>/dev/null | sed -rn 's/.*COVERAGE_OUTPUT_FILE=(.*)\/coverage\.dat.*/\1/p')
 OUTFILE=$2
-
 
 IFS=$'\n' CPATHS=($PATHS_STR)
 
@@ -19,16 +19,16 @@ IFS=$' ' UCPATHS=($(printf "%s " "${CPATHS[@]}" | sort -u))
 
 # start adding tracefiles together
 rm -f "$OUTFILE"
-set +x
 for CPATH in "${UCPATHS[@]}"
 do
     if [ -d "$CPATH" ]; then
         CFILE="$CPATH/coverage.dat"
-        echo "+processing file $CFILE"
-        lcov --remove "$CFILE" 'external/*' -o "/tmp/$OUTFILE"
+        echo "Processing file $CFILE"
+        lcov --remove "$CFILE" 'external/*' '**/test/*' -o "/tmp/$OUTFILE"
         if [ -f "$OUTFILE" ]; then
-            lcov "$OUTFILE" "/tmp/$OUTFILE" -o "/tmp/$OUTFILE"
+            lcov -a "$OUTFILE" -a "/tmp/$OUTFILE" -o "/tmp/$OUTFILE"
         fi
         mv "/tmp/$OUTFILE" "$OUTFILE"
+        lcov --list $OUTFILE
     fi
 done
