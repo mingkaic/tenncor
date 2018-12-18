825e2a4ccba7516178412b4dfa2e1fb745d3ec44
RaggleDodo <rododorodo@gmail.com>
2018-12-17 22:40:54 -0800

    clean and update docs

--- docs/html/search/all_f.js
+++ docs/html/search/all_f.js
@@ -5,8 +5,8 @@ var searchData=
   ['tensor_5f',['tensor_',['../structade_1_1_mapped_tensor.html#aabd63daa33eaff88d036361a7d6d0343',1,'ade::MappedTensor']]],
   ['tensptrt',['TensptrT',['../namespaceade.html#aceac6eb9293c8b2001737f3b26667993',1,'ade']]],
   ['tensreft',['TensrefT',['../namespaceade.html#a0cd5d0a139b258e530ac3140bc7ccbb1',1,'ade']]],
-  ['tenst',['TensT',['../namespaceage.html#a216064d535ba55c348ad0df8671ee5b3',1,'age']]],
-  ['to_5fargs',['to_args',['../namespaceage.html#afcd027e02c955a45b931401f1f298913',1,'age']]],
+  ['tenst',['TensT',['../namespaceade.html#ab4db19108073a8a4dc5b8cb6aa85ad42',1,'ade']]],
+  ['to_5fargs',['to_args',['../namespaceage.html#a1544ab48782ca9c72b56e4bdb23a96e2',1,'age']]],
   ['to_5fstring',['to_string',['../structade_1_1i_coord_map.html#a1303dc91fea22702ba403732ba25227f',1,'ade::iCoordMap::to_string()'],['../structade_1_1_coord_map.html#a35bd718362918b31a1f0f2a86c2a3c3b',1,'ade::CoordMap::to_string()'],['../structade_1_1_functor.html#a4503e570f95a289825384be36689be9a',1,'ade::Functor::to_string()'],['../structade_1_1i_tensor.html#a01138fc6bce780fcd4b4e495ca050fce',1,'ade::iTensor::to_string()'],['../structade_1_1_shape.html#a92348dfc2c555cae7d1d42a48f59bd6d',1,'ade::Shape::to_string()'],['../namespaceade.html#adf0ae4c17a6835d771a3e8e1502f9ad6',1,'ade::to_string()']]],
   ['traveler_2ehpp',['traveler.hpp',['../traveler_8hpp.html',1,'']]],
   ['type_5fcode',['type_code',['../structade_1_1i_leaf.html#ae8d730d1d4a0905af079930eb88ecd6b',1,'ade::iLeaf']]]
