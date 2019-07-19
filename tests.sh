58c4eff7183990cccc5e24e965b761a06f988966
RaggleDodo <rododorodo@gmail.com>
2019-07-19 09:19:55 -0700

    address ctest linking issue in docker test container

--- tests.sh
+++ tests.sh
@@ -10,21 +10,21 @@ set -e
 # ===== Run Gtest =====
 echo "===== TESTS =====";
 
-bazel test --config asan --config gtest --action_env="ASAN_OPTIONS=detect_leaks=0" //ade:test
+bazel test --config asan --config gtest --sandbox_debug --action_env="ASAN_OPTIONS=detect_leaks=0" //ade:test
 bazel test --run_under='valgrind --leak-check=full' //ade:test
 
-bazel test --config asan --config gtest --action_env="ASAN_OPTIONS=detect_leaks=0" //tag:test
+bazel test --config asan --config gtest --sandbox_debug --action_env="ASAN_OPTIONS=detect_leaks=0" //tag:test
 bazel test --run_under='valgrind --leak-check=full' //tag:test
 
-bazel test --config asan --config gtest --action_env="ASAN_OPTIONS=detect_leaks=0" //pbm:test
+bazel test --config asan --config gtest --sandbox_debug --action_env="ASAN_OPTIONS=detect_leaks=0" //pbm:test
 bazel test --run_under='valgrind --leak-check=full' //pbm:test
 
-bazel test --config asan --config gtest --action_env="ASAN_OPTIONS=detect_leaks=0" //opt:test
+bazel test --config asan --config gtest --sandbox_debug --action_env="ASAN_OPTIONS=detect_leaks=0" //opt:test
 bazel test --run_under='valgrind --leak-check=full' //opt:test
 
 bazel test --run_under='valgrind --leak-check=full' //gen:ptest
 
-bazel test --config asan --config gtest --action_env="ASAN_OPTIONS=detect_leaks=0" //ead:ctest
+bazel test --config asan --config gtest --sandbox_debug --action_env="ASAN_OPTIONS=detect_leaks=0" //ead:ctest
 bazel test --run_under='valgrind --leak-check=full' //ead:ctest
 bazel test --run_under='valgrind --leak-check=full' //ead:ptest
 
