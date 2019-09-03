c3ff17ef2968b594bb12a99f4b46be1210ec9e9a
RaggleDodo <rododorodo@gmail.com>
2019-09-02 17:11:15 -0700

    reduce ead build time

--- tests.sh
+++ tests.sh
@@ -13,16 +13,16 @@ free -m;
 # ===== Run Gtest =====
 echo "===== TESTS =====";
 
-bazel test --config asan --config gtest --action_env="ASAN_OPTIONS=detect_leaks=0" --jobs=4 \
+bazel test --config asan --config gtest --action_env="ASAN_OPTIONS=detect_leaks=0" --define EAD_CFG=MIN \
 //ade:test //tag:test //pbm:test //opt:test //opt/parse:test //ead:ctest //perf:test //pll:test
 
-bazel test --run_under='valgrind --leak-check=full' --jobs=4 \
+bazel test --run_under='valgrind --leak-check=full' --define EAD_CFG=MIN \
 //ade:test //gen:ptest //tag:test //pbm:test //opt:test //opt/parse:test //ead:ctest //ead:ptest //perf:test //pll:test
 
 # ===== Coverage Analysis ======
 echo "===== STARTING COVERAGE ANALYSIS =====";
 bazel coverage --config asan --action_env="ASAN_OPTIONS=detect_leaks=0" \
---config gtest --config cc_coverage --jobs=4 \
+--config gtest --config cc_coverage \
 //ade:test //tag:test //pbm:test //opt:test //opt/parse:test //ead:ctest //perf:test //pll:test
 lcov --remove bazel-out/_coverage/_coverage_report.dat \
 'external/*' '**/test/*' 'testutil/*' '**/genfiles/*' 'dbg/*' \
