fix cnn demo with correct relu and stable softmax

eteq/test/ptest.py
@@ -423,7 +423,7 @@ class EADTest(unittest.TestCase):
         if 'elementary.shape' in _test_data:
             shapes += _test_data['elementary.shape']
         for shape in shapes:
-            self._common_unary_tf(shape, tc.nn.relu, tf.nn.relu)
+            self._common_unary_tf(shape, tc.relu, tf.nn.relu)
 
     def test_square(self):
         shapes = [[3, 4, 5]]

rocnnet/demo/cnn_demo.py
@@ -31,6 +31,7 @@ learning_rate = 0.01
 momentum = 0.9
 weight_decay = 0.0001
 show_every_n = 5
+nepochs = 10
 
 ds = tfds.load('cifar10',
     split=tfds.Split.TRAIN,
@@ -59,7 +60,7 @@ model.add(rcn.maxpool2d([1, 2])) # outputs [nbatch, 4, 4, 20]
 model.add(rcn.Dense(10, 320,
     weight_init=rcn.unif_xavier_init(),
     bias_init=rcn.zero_init(), label="fc")) # outputs [nbatch, 10]
-model.add(rcn.softmax(0))
+model.add(rcn.softmax(1))
 
 sess = eteq.Session()
 
@@ -74,10 +75,11 @@ sess.track([
 raw_inshape[0] = nbatch
 train_inshape = raw_inshape
 train_outshape = [nbatch, 10]
-train_input = eteq.Variable(train_inshape)
-train_output = eteq.Variable(train_outshape)
+train_input = eteq.Variable(train_inshape, label="trainin")
+train_output = eteq.Variable(train_outshape, label="trainout")
+normalized = train_input / 255 - 0.5
 train = rcn.sgd_train(model, sess,
-    train_input, train_output, rcn.get_sgd(0.9))
+    normalized, train_output, rcn.get_sgd(0.5))
 
 sess.optimize("cfg/optimizations.rules")
 
@@ -86,14 +88,19 @@ for i, data in enumerate(cifar):
     labels = np.zeros((nbatch, 10))
     for j, label in enumerate(data['label']):
         labels[j][label] = 1
+    print('expected label:\n{}'.format(labels))
     train_input.assign(data['image'].astype(np.float))
     train_output.assign(labels.astype(np.float))
-    trained_err = train()
-    print('done episode {}'.format(i))
-    if i % show_every_n == show_every_n - 1:
-        err = trained_err.as_numpy()
-        print('training {}\ntraining error:\n{}'
-            .format(i + 1, err))
+    for j in range(nepochs):
+        trained_err = train()
+        print('done epoch {}'.format(j))
+        if j % show_every_n == show_every_n - 1:
+            guess_err = trained_err.as_numpy()
+            err = np.max(guess_err, axis=1)
+            print(('training {}th image, epoch {}\n'+
+                'training error:\n{}\n'+
+                'training error max:\n{}')
+                .format(i, j + 1, guess_err, err))
 
 # test
 tds = tfds.load('cifar10',