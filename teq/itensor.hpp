ca72f418a31343b8c23b0c4e5399cd230bad324c
RaggleDodo <rododorodo@gmail.com>
2020-07-19 18:02:49 -0700

    move traveler interface to own file

--- teq/itensor.hpp
+++ teq/itensor.hpp
@@ -10,28 +10,13 @@
 #include <unordered_set>
 
 #include "teq/shape.hpp"
+#include "teq/itraveler.hpp"
 
 #ifndef TEQ_ITENSOR_HPP
 #define TEQ_ITENSOR_HPP
 
 namespace teq
 {
-
-struct iLeaf;
-
-struct iFunctor;
-
-/// Interface to travel through graph, treating iLeaf and iFunctor differently
-struct iTraveler
-{
-	virtual ~iTraveler (void) = default;
-
-	/// Visit leaf node
-	virtual void visit (iLeaf& leaf) = 0;
-
-	/// Visit functor node
-	virtual void visit (iFunctor& func) = 0;
-};
 
 struct iDeviceRef
 {
