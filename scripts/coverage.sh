0eb0166da1c8419943a9a36e458f6309bab7af20
RaggleDodo <rododorodo@gmail.com>
2020-10-12 00:04:46 -0700

    remove bazel coverage filtering

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -13,7 +13,7 @@ function bzl_coverage() {
 	--action_env="ASAN_OPTIONS=detect_leaks=0" \
 	--config asan \
 	--config cc_coverage \
-	--remote_http_cache="$REMOTE_CACHE" $@ | grep -v -E '+|Processing';
+	--remote_http_cache="$REMOTE_CACHE" $@;
 	# extract coverage from bazel cache
 	COV_FILE="coverage.info";
 	if ! [ -z "$COV_DIR" ];
